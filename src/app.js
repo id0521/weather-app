a5bdf9c198d1bb70829adc8a7588750be1884316
ivand521 <ivandong521@gmail.com>
2019-07-10 13:34:10 -0400

    init commit

---
+++ src/app.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode.js');
+const forecast = require('./utils/forecast.js');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//set up handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Set up static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Ivan Dong'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Ivan Dong'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'This page provides the answers for FAQs',
+    title: 'Help',
+    name: 'Ivan Dong'
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address!'
+    });
+  }
+   geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+     if (error) {
+       return res.send({ error });
+     }
+
+     forecast(latitude, longitude, (error, forecastData) => {
+       if (error) {
+         return res.send({ error });
+       }
+
+       res.send({
+         forecast: forecastData ,
+         location,
+         address: req.query.address
+       });
+     });
+   });
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: []
+  });
+})
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: 404,
+    error: 'Help article not found.',
+    name: 'Ivan Dong'
+  })
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    error: 'Page not found',
+    name: 'Ivan Dong'
+  })
+});
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
+});
